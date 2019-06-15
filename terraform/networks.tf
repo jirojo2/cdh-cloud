5f31772678883772e67e6efded9dfe7c8a3c1bcf
Jose Ignacio Rojo Rivero <jirojo2@gmail.com>
2019-06-15 12:59:59 +0200

    upgraded to terraform 0.12 syntax

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -1,29 +1,30 @@
 resource "azurerm_resource_group" "terraform_rg" {
-  name 		= "${var.resource_group_name}"
-  location 	= "${var.location}"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name 			= "cdh-vnet"
-  address_space 	= ["${var.vnet_cidr}"]
-  location 		= "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
+  name                = "cdh-vnet"
+  address_space       = [var.vnet_cidr]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.terraform_rg.name
 
-  tags {
-	group = "Coudera-Cluster"
+  tags = {
+    group = "Coudera-Cluster"
   }
 }
 
 resource "azurerm_subnet" "subnet_1" {
-  name 			= "Subnet-1"
-  address_prefix 	= "${var.subnet1_cidr}"
-  virtual_network_name 	= "${azurerm_virtual_network.vnet.name}"
-  resource_group_name 	= "${azurerm_resource_group.terraform_rg.name}"
+  name                 = "Subnet-1"
+  address_prefix       = var.subnet1_cidr
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.terraform_rg.name
 }
 
 resource "azurerm_subnet" "subnet_2" {
-  name 			= "Subnet-2"
-  address_prefix 	= "${var.subnet2_cidr}"
-  virtual_network_name 	= "${azurerm_virtual_network.vnet.name}"
-  resource_group_name 	= "${azurerm_resource_group.terraform_rg.name}"
+  name                 = "Subnet-2"
+  address_prefix       = var.subnet2_cidr
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.terraform_rg.name
 }
+
--- terraform/vm_prop.tf
+++ terraform/vm_prop.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_machine" "la_manager" {
   name                  = "cdh_manager"
-  location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.public_nic.id}"]
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.terraform_rg.name
+  network_interface_ids = [azurerm_network_interface.public_nic.id]
   vm_size               = "Standard_DS3_v2"
 
-#This will delete the OS disk automatically when deleting the VM
+  #This will delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
 
   storage_image_reference {
@@ -13,7 +13,6 @@ resource "azurerm_virtual_machine" "la_manager" {
     offer     = "CentOS"
     sku       = "7.2"
     version   = "7.2.20160303"
-
   }
 
   storage_os_disk {
@@ -23,42 +22,39 @@ resource "azurerm_virtual_machine" "la_manager" {
     create_option = "FromImage"
   }
 
-
   # Optional data disks
-    storage_data_disk {
-      name          = "data"
-      vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data1.vhd"
-      disk_size_gb  = "100"
-      create_option = "Empty"
-      lun           = 0
-    }
+  storage_data_disk {
+    name          = "data"
+    vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data1.vhd"
+    disk_size_gb  = "100"
+    create_option = "Empty"
+    lun           = 0
+  }
 
   os_profile {
     computer_name  = "manager"
-    admin_username = "${var.vm_username_manager}"
-    admin_password = "${var.vm_password}"
+    admin_username = var.vm_username_manager
+    admin_password = var.vm_password
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
+  }
 
-}
-
-  tags {
+  tags = {
     group = "OpenLogic-Cluster"
   }
 }
 
-
 # Master (High Availability)
 resource "azurerm_virtual_machine" "la_master" {
   name                  = "cdh_master"
-  location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.public_nic1.id}"]
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.terraform_rg.name
+  network_interface_ids = [azurerm_network_interface.public_nic1.id]
   vm_size               = "Standard_DS3_v2"
 
-#This will delete the OS disk automatically when deleting the VM
+  #This will delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
 
   storage_image_reference {
@@ -66,7 +62,6 @@ resource "azurerm_virtual_machine" "la_master" {
     offer     = "CentOS"
     sku       = "7.2"
     version   = "7.2.20160303"
-
   }
 
   storage_os_disk {
@@ -77,39 +72,38 @@ resource "azurerm_virtual_machine" "la_master" {
   }
 
   # Optional data disks
-    storage_data_disk {
-      name          = "data"
-      vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data2.vhd"
-      disk_size_gb  = "100"
-      create_option = "Empty"
-      lun           = 0
-    }
+  storage_data_disk {
+    name          = "data"
+    vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data2.vhd"
+    disk_size_gb  = "100"
+    create_option = "Empty"
+    lun           = 0
+  }
 
   os_profile {
     computer_name  = "master"
-    admin_username = "${var.vm_username_master}"
-    admin_password = "${var.vm_password}"
+    admin_username = var.vm_username_master
+    admin_password = var.vm_password
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
   }
 
-  tags {
+  tags = {
     group = "OpenLogic-Cluster"
   }
 }
 
-
 # Worker 1
 resource "azurerm_virtual_machine" "la_worker1" {
   name                  = "cdh_worker1"
-  location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.public_nic2.id}"]
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.terraform_rg.name
+  network_interface_ids = [azurerm_network_interface.public_nic2.id]
   vm_size               = "Standard_DS2_v2"
 
-#This will delete the OS disk automatically when deleting the VM
+  #This will delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
 
   storage_image_reference {
@@ -127,25 +121,25 @@ resource "azurerm_virtual_machine" "la_worker1" {
   }
 
   # Optional data disks
-    storage_data_disk {
-      name          = "data"
-      vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data3.vhd"
-      disk_size_gb  = "100"
-      create_option = "Empty"
-      lun           = 0
-    }
+  storage_data_disk {
+    name          = "data"
+    vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data3.vhd"
+    disk_size_gb  = "100"
+    create_option = "Empty"
+    lun           = 0
+  }
 
   os_profile {
     computer_name  = "worker1"
-    admin_username = "${var.vm_username_worker1}"
-    admin_password = "${var.vm_password}"
+    admin_username = var.vm_username_worker1
+    admin_password = var.vm_password
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  tags {
+  tags = {
     group = "OpenLogic-Cluster"
   }
 }
@@ -153,12 +147,12 @@ resource "azurerm_virtual_machine" "la_worker1" {
 # Worker 2
 resource "azurerm_virtual_machine" "la_worker2" {
   name                  = "cdh_worker2"
-  location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.public_nic3.id}"]
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.terraform_rg.name
+  network_interface_ids = [azurerm_network_interface.public_nic3.id]
   vm_size               = "Standard_DS2_v2"
 
-#This will delete the OS disk automatically when deleting the VM
+  #This will delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
 
   storage_image_reference {
@@ -176,25 +170,25 @@ resource "azurerm_virtual_machine" "la_worker2" {
   }
 
   # Optional data disks
-    storage_data_disk {
-      name          = "data"
-      vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data4.vhd"
-      disk_size_gb  = "100"
-      create_option = "Empty"
-      lun           = 0
-    }
+  storage_data_disk {
+    name          = "data"
+    vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data4.vhd"
+    disk_size_gb  = "100"
+    create_option = "Empty"
+    lun           = 0
+  }
 
   os_profile {
     computer_name  = "worker2"
-    admin_username = "${var.vm_username_worker2}"
-    admin_password = "${var.vm_password}"
+    admin_username = var.vm_username_worker2
+    admin_password = var.vm_password
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  tags {
+  tags = {
     group = "OpenLogic-Cluster"
   }
 }
@@ -202,12 +196,12 @@ resource "azurerm_virtual_machine" "la_worker2" {
 # Worker 3
 resource "azurerm_virtual_machine" "la_worker3" {
   name                  = "cdh_worker3"
-  location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.public_nic4.id}"]
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.terraform_rg.name
+  network_interface_ids = [azurerm_network_interface.public_nic4.id]
   vm_size               = "Standard_DS2_v2"
 
-#This will delete the OS disk automatically when deleting the VM
+  #This will delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
 
   storage_image_reference {
@@ -225,25 +219,26 @@ resource "azurerm_virtual_machine" "la_worker3" {
   }
 
   # Optional data disks
-    storage_data_disk {
-      name          = "data"
-      vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data5.vhd"
-      disk_size_gb  = "100"
-      create_option = "Empty"
-      lun           = 0
-    }
+  storage_data_disk {
+    name          = "data"
+    vhd_uri       = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}/data5.vhd"
+    disk_size_gb  = "100"
+    create_option = "Empty"
+    lun           = 0
+  }
 
   os_profile {
     computer_name  = "worker3"
-    admin_username = "${var.vm_username_worker3}"
-    admin_password = "${var.vm_password}"
+    admin_username = var.vm_username_worker3
+    admin_password = var.vm_password
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  tags {
+  tags = {
     group = "OpenLogic-Cluster"
   }
 }
+
